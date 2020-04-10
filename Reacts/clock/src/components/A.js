ffa5bc2e31285b0790a207031be79c52b4060da1
Sheenam-khan <55406374+Sheenam-khan@users.noreply.github.com>
2020-04-10 10:50:51 +0530

    Update A.js

--- Reacts/clock/src/components/A.js
+++ Reacts/clock/src/components/A.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import B from './B'
 class A extends Component {
-    
+    constructor(props) {
+        super(props)
+      this.currentRef=React.createRef();
+    }
     accessChild=()=>{
         console.log(this.refs.child)
-        this.refs.child.increment()
+         this.currentRef.current.increment()
+      //  this.refs.child.increment()
     }
     
     render() {
         return (
             <div>
               
-            <B ref="child"/>   
+       {/* <Child ref="child"/>    */}
+            <B ref={this.currentRef}/>   
             (parent)A<button onClick={this.accessChild}>count</button>
             </div>
         )
