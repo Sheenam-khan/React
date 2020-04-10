db80c4aedf509078e910958617c51d01b3aeaf26
Sheenam-khan <55406374+Sheenam-khan@users.noreply.github.com>
2020-04-10 11:43:10 +0530

    Update App.js
    clock and p-c communication

--- Reacts/clock/src/App.js
+++ Reacts/clock/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Clock from './components/Clock';
+import A from './components/A';
  
 function App() {
   return (
     <div className="App">
    <Clock></Clock>
+   <br><br>
+   <h1>3rd task<h1>
+   <A />
     </div>
   );
 }
