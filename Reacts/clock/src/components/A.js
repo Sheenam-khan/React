27941f197ba6dbc19823086e58f2956394fbf99d
Sheenam-khan <55406374+Sheenam-khan@users.noreply.github.com>
2020-04-12 03:38:48 +0530

    Update A.js

--- Reacts/clock/src/components/A.js
+++ Reacts/clock/src/components/A.js
@@ -25,33 +25,6 @@ class A extends Component {
 }
 
 export default A
-
-=======
-import React, { Component } from 'react'
-import B from './B'
-class A extends Component {
-    constructor(props) {
-        super(props)
-      this.currentRef=React.createRef();
-    }
-    accessChild=()=>{
-       // console.log(this.refs.child)
-         this.currentRef.current.increment()
-     // this.refs.child.increment()
-    }
-    
-    render() {
-        return (
-            <div>
-              
-       {/* <Child ref="child"/>    */}
-            <B ref={this.currentRef}/>   
-            (parent)A<button onClick={this.accessChild}>count</button>
-            </div>
-        )
-    }
-}
-
-export default A
+ 
 
  
