3515026f1856c153c3e2184c3c7cb1e516d445e3
Sheenam-khan <55406374+Sheenam-khan@users.noreply.github.com>
2020-04-12 04:03:43 +0530

    Update App.js

--- Reacts/clock/src/App.js
+++ Reacts/clock/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.css';
- 
+
 import Clock2 from './components/Clock2';
- 
-import Clock from './components/Clock';
- 
+
 import A from './components/A';
  
 function App() {
   return (
     <div className="App">
+
+    <Clock2></Clock2>
  
-   <Clock2></Clock2>
- 
- <A/>
- 
-   <Clock></Clock>
-   <br><br>
-   <h1>3rd task<h1>
+  
+   <br></br>
+   <h1>3rd task</h1>
    <A />
  
     </div>
